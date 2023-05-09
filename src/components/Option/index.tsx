9cc09597f1000c0f4c3af6ffa997cd19e97a4b99
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-05-09 07:17:54 -0300

    Animando com Skia

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,8 +1,12 @@
+import { useEffect } from 'react';
 import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
 import {
+  BlurMask,
   Canvas,
   Path,
   Skia,
+  runTiming,
+  useValue,
 } from '@shopify/react-native-skia';
 
 import { THEME } from '../../styles/theme';
@@ -17,9 +21,19 @@ const CHECK_SIZE = 28;
 const CHECK_STROKE = 2;
 
 export function Option({ checked, title, ...rest }: Props) {
+  const percentage = useValue(0);
+
   const RADIUS = (CHECK_SIZE - CHECK_STROKE) / 2;
   const path = Skia.Path.Make();
   path.addCircle(CHECK_SIZE, CHECK_SIZE, RADIUS);
+
+  useEffect(() => {
+    if (checked) {
+      runTiming(percentage, 1, { duration: 700 });
+    } else {
+      runTiming(percentage, 0, { duration: 700 });
+    }
+  }, [checked]);
 
   return (
     <TouchableOpacity
@@ -49,8 +63,10 @@ export function Option({ checked, title, ...rest }: Props) {
           style="stroke"
           strokeWidth={CHECK_STROKE}
           start={0}
-          end={0.5}
-        />
+          end={percentage}
+        >
+          <BlurMask blur={1} style="solid" />
+        </Path>
       </Canvas>
     </TouchableOpacity>
   );
